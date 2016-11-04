2accf04ebeafb67218eef78733f39a020f420842
HeikkiK <heikki.kortetmaa@hotmail.com>
2016-11-05 00:00:29 +0200

    Still testing 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	private Rigidbody2D rb2d;
+	public float speed = 10;
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
 
     void FixedUpdate()
     {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
     }
 }
